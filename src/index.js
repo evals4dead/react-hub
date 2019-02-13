6f03d1c93245c33138af57206c64bbb245b127a9
DonghoChoi <donghochoi@DonghoChoiui-MacBookPro.local>
2019-02-13 16:25:28 +0900

    basic styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './components/Router';
+import './styles/base.scss';
 import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById('root');
